44cb01b453dedf4c081b1db6c912ea39073ac481
BobbyKJH <rlatnguscjsw@gmail.com>
2022-05-02 16:37:15 +0900

    start food-community web

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import styles from "./Nav.module.css";
+
+export default function Nav() {
+  return (
+    <div className={styles.Nav}>
+      <div>1</div>
+      <div className={styles.name}>FOOD - COMMUNITY</div>
+      <div className={styles.subNav}>1</div>
+    </div>
+  );
+}
