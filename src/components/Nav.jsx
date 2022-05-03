56ea0fa58a773cb89197d62d7a7d1c075895f2de
BobbyKJH <rlatnguscjsw@gmail.com>
2022-05-03 23:27:25 +0900

    fixed Nav Bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,43 +3,52 @@ import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 
 export default function Nav() {
-  const [subNav, setSubNav] = useState("relative");
-
-  const scroll = () => {
-    setSubNav("fixed");
-  };
   return (
-    <div className={styles.Nav}>
-      {/* 로그인 */}
-      <div className={styles.find}>
-        <Link to="/" className={styles.findLink}>
-          로그인
-        </Link>
-        <hr />
-        <Link to="/" className={styles.findLink}>
-          회원가입
-        </Link>
-      </div>
-      {/* 제목 */}
-      <div className={styles.name}>
-        <Link className={styles.titleLink} to="/">
-          FOOD COMMUNITY
-        </Link>
-      </div>
-      {/* fix 타이틀 */}
-      <div className={styles.subNav} style={{ position: subNav }}>
-        <Link className={styles.contentLink} to="/">
-          게시판
-        </Link>
-        <Link className={styles.contentLink} to="/">
-          1
-        </Link>
-        <Link className={styles.contentLink} to="/">
-          1
-        </Link>
-        <Link className={styles.contentLink} to="/">
-          1
-        </Link>
+    <div className={styles.body}>
+      {/* 박스 */}
+      <div className={styles.nav}>
+        <hr className={styles.line} />
+        {/* 제목 */}
+        <div className={styles.titleNav}>
+          <hr />
+          <Link className={styles.titleLink} to="/">
+            나만의 음식찾기
+          </Link>
+          <hr />
+        </div>
+
+        <hr className={styles.line} />
+
+        {/* fix 타이틀 */}
+        <div className={styles.subNav}>
+          <Link className={styles.contentLink} to="/">
+            먹거리 찾기
+          </Link>
+          <Link className={styles.contentLink} to="/">
+            먹거리 추천
+          </Link>
+          <Link className={styles.contentLink} to="/">
+            게시판
+          </Link>
+          <Link className={styles.contentLink} to="/">
+            1
+          </Link>
+        </div>
+
+        <hr className={styles.line} />
+
+        {/* 로그인 */}
+        <div className={styles.findNav}>
+          <Link to="/" className={styles.findLink}>
+            로그인
+          </Link>
+          <hr />
+          <Link to="/" className={styles.findLink}>
+            회원가입
+          </Link>
+        </div>
+
+        <hr className={styles.line} />
       </div>
     </div>
   );
